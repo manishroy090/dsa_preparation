492b21e7099f9eab34aba9610c78e960825b4eec
manishroy090 <89403173+manishroy090@users.noreply.github.com>
2026-06-12 09:23:03 -0400

    implemented_reverse_string

---
+++ Strings/Reverse.js
@@ -0,0 +1,26 @@
+function reverseString(strVal){
+
+    let left =0;
+
+    let right = strVal.length-1;
+
+    let result = []
+
+    while(left < right){
+
+       result.push(strVal[right]);
+
+       right--;
+
+    }
+
+    return result;
+  
+}
+
+
+const strVal= "manish"
+
+const result = reverseString(strVal);
+
+console.log("result",result);
