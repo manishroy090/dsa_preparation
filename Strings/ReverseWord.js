e984fdba2d2e6c5a090647f5ecfeffa3eab3fdef
manishroy090 <89403173+manishroy090@users.noreply.github.com>
2026-06-20 04:08:36 -0400

    solved_geeks_for_geeks_reversed_word_in_string_with_expected_approch_with_o(n)time_o(1)space

--- Strings/ReverseWord.js
+++ Strings/ReverseWord.js
@@ -1,48 +1,38 @@
 function reverse(s, start, end) {
   while (start < end) {
-    //store start value on temp
-    let temp = s[start];
-    //store end value on start
-    s[start] = s[end];
-    //now store the start value that store on the tem on end
-    s[end] = temp;
+    let tmp = s[end];
+    s[end] = s[start];
+    s[start] = tmp;
+
     start++;
     end--;
   }
 
-  return (s[start] = s[end]);
 }
-function reverseWords(s) {
-  //if we write space its converted to one index
-  s = s.split("");
 
+function reverseWords(s) {
+  s = s.split("");
   reverse(s, 0, s.length - 1);
 
   let n = s.length;
-
   let i = 0;
 
-  let check = [];
-
   for (let l = 0; l < n; ++l) {
-    //not equal to . constraint
 
-    if (s[l] !== ".") {
-      // console.log(s[l]);
-      if(i!==0) s[i++] = '.';
+     if(s[l] !=='.'){
+        if(i!==0) s[i++]='.'
 
-       let r = l;
+        let r = l
 
-      
-       while(r < n && s[r]!=='.') s[i++]
+        while(r < n && s[r]!=='.') s[i++] =s[r++];
+        reverse(s,i-(r-l),i-1);
+        l=r;
 
-
-    }
+     }
   }
 
-  // return check;
+  return s.slice(0,i).join('');
 }
-
 // let s = "i.like.this.program.very.much";
 let s = "..geeks..for.geeks.";
 // let s = "...home......";
